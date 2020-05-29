027487bcdc7811bb8a89265d829da842a20f8052
KaiBartolone <kbartolo@stanford.edu>
2020-05-29 00:36:12 -0400

    git commit

--- src/include/dualshock.h
+++ src/include/dualshock.h
@@ -3,12 +3,18 @@
 
 #include <linux/joystick.h>
 #include <fcntl.h>
+#include <string>
+#include <unordered_map>
+#include "button.h"
+#include "trigger.h"
+#include "axis.h"
+#include "touchpad.h"
 
 class Gamepad
 {
     public:
         /**
-        * Creates instance of gamepad. If dualshock controller canntot be found, returns false.
+        * Creates instance of gamepad. If dualshock controller cannot be found, returns false.
         */
         Gamepad(int fd);
 
@@ -16,8 +22,51 @@ class Gamepad
         * Cleans up all memory allocated for gamepad
         */
         ~Gamepad();
+
+        /**
+          * Begins thread that continously reads from /dev/js0 to update gamepad values and 
+        */
+        void startPolling();
+
+        /**
+         * Returns gamepad button with provided name.
+        */
+        Button getButton(string name);
+
+        /**
+         * Returns gamepad button state with provided name.
+        */
+        bool getButtonState(string name);
+
+        /**
+         * Returns gamepad trigger with provided id.
+        */
+        Trigger getTrigger(int id);
+
+        /**
+         * Returns value of gamepad trigger with provided id.
+        */
+        float getTriggerValue(int id);
+
+        /**
+         * Returns state gamepad trigger with provided id.
+        */
+        float getTriggerState(int id);
+
+        /**
+         * Returns gamepad axis with provided id.
+        */
+        Axis getAxis(int id);
+
+        /**
+         * Returns values of gamepad axis with provided id.
+        */
+        Point<float x, float y> getAxisState(int id);
+
     private:
         int id;
+        const unordered_map<string, Button> buttons;
+        bool isPolling = false;
 };
 
 #endif
--- src/libs/dualshock.cpp
+++ src/libs/dualshock.cpp
@@ -8,4 +8,17 @@ Gamepad::Gamepad(int fd)
 Gamepad::~Gamepad()
 {
     //do nothing
+
+    //stop polling from happening
+}
+
+Gamepad::start()
+{
+    //error checking in case the controller is not connected and the coder tries to begin polling
+    isPolling = true;
+    while (isPolling)
+    {
+        //update values of gamepad things
+        //make sure event listeners are happening
+    }
 }
