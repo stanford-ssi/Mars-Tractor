05e66833afc14bf4a047829f90132ed40a68ac3b
ssi <matthtan@stanford.edu>
2021-03-29 23:31:36 -0400

    fixed thread issues, cleaned up chassis class

--- roverOS/chassis.py
+++ roverOS/chassis.py
@@ -46,8 +46,8 @@ class Motor:
     position = 0
     
     PID_TIME = .1
-    PV=.05
-    IV=.7
+    PV=.055
+    IV=.68
     DV=.001
 
     def __init__(self, pin_pwm, pin_a, pin_b, pca_object, encoder_a, encoder_b):
@@ -59,6 +59,9 @@ class Motor:
         self.pidV = PID(self.PV, self.IV, self.DV)
         self.pidV.output_limits=(-100,100)
         self.pid_active = False
+        self.active = True
+        threading.Thread(target=self.__get_velocity).start()
+
 
 
     def set_power(self, power):
@@ -78,11 +81,18 @@ class Motor:
     def get_position(self):
         return self.encoder.get_position()
 
-    def __velocity_PID(self, velocity):
+    def __get_velocity(self):
+        old_position = self.get_position()
+        while self.active:
+            self.current_velocity = (self.get_position()-old_position)/self.PID_TIME
+            old_position = self.get_position()
+            time.sleep(self.PID_TIME)
+
+    def __velocity_PID(self, velocity):#using it's own velocity calculation because of thread sync issues
         self.pidV.setpoint = velocity
         old_position = self.get_position()
         while self.pid_active:
-            self.current_velocity = (self.get_position()-old_position)/self.PID_TIME
+            current_velocity = (self.get_position()-old_position)/self.PID_TIME
             old_position = self.get_position()
             command = self.pidV(self.current_velocity)
             self.set_power(command)
@@ -98,6 +108,10 @@ class Motor:
 
     def cancel_pid(self):
         self.pid_active=False
+
+    def shutdown(self):
+        self.pid_active=False
+        self.active=False
         
 
 class Chassis:
@@ -116,46 +130,47 @@ class Chassis:
     pca2 = PCA9685(i2c, address=0x41)
     pca.frequency = 50
     pca2.frequency = 50
-
-    def getPosition(self):
-        return self.b1.get_position()
-
-    def setVelocity(self, velocity):
-        self.b1.set_velocity(velocity)
-
-    def getVelocity(self):
-        return self.b1.get_velocity()
     
     a1 = Motor(15,14,13,pca2, 'DAP1_FS', 'GPIO_PQ4')
     b1 = Motor(12,11,10,pca2, 'GPIO_PQ5', 'CAN_GPIO2')
     a2 = Motor(9,8,7,pca2, 'DAP1_DIN', 'DAP1_DOUT')
-    b2 = Motor(6,4,5,pca2, 'UART1_CTS', 'CAN_GPIO1')
-    a3 = Motor(1,2,3,pca2, 'CAN_GPIO0', 'GPIO_MDM2')
-    b3 = Motor(15,14,13,pca, 'GPIO_CAM5', 'GPIO_CAM4')
+    b2 = Motor(6,4,5,pca2, 'GPIO_AUD0', 'AUD_MCLK')
+    #a3 = Motor(1,2,3,pca2, 'CAN_GPIO0', 'GPIO_MDM2')
+    #b3 = Motor(15,14,13,pca, 'GPIO_CAM5', 'GPIO_CAM4')
 
-    motors = [a1,b1,a2,b2,a3,b3]
+    motors = [a1,b1,a2,b2]
 
     def drive(self, power):
         for motor in self.motors:
             motor.set_power(power)
     
+    def set_velocity(self, velocity):
+        for motor in self.motors:
+            motor.set_velocity(velocity)
+    
     def stop_all(self):
-        self.drive(0)
         for motor in self.motors:
             motor.cancel_pid()
+        self.drive(0)
 
-    def getVelocity(self):
+    def get_velocities(self):
+        return [motor.get_velocity() for motor in self.motors]
+
+    def get_positions(self):
+        return [motor.get_position() for motor in self.motors]
+    
+    def shutdown(self):
+        self.stop_all()
         for motor in self.motors:
-            print(motor.get_velocity())
-        print('--------------')
+            motor.shutdown()
     
     
 
 chassis = Chassis()
-chassis.drive(0)
-"""
-for _ in range(10):
-    chassis.getVelocity()
-    time.sleep(1)
-chassis.stop_all()
-"""
+try:
+    chassis.set_velocity(250)
+    for _ in range(20):
+        print(chassis.get_velocities())
+        time.sleep(1)
+finally:
+    chassis.shutdown()
